34de541d41eab5122a243d28061d7ad88a654314
Natalia Sundström <natalia.sundstrom@studerande.karlskogafhs.se>
2022-05-20 17:17:23 +0200

    Add slider to hero

---
+++ js/slider.js
@@ -0,0 +1,54 @@
+const currentSlideEl = document.querySelector('.slider__current')
+const slides = document.querySelectorAll('.swiper-slide')
+const currentAnchorEl = document.querySelector('.slider__anchor')
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'fade',
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider__arrow--right',
+      prevEl: '.slider__arrow--left',
+    },
+
+    // Change number in pagination 
+   on: {
+       transitionEnd: function () {
+
+        // Set current slide number 
+        let currentIndex = this.realIndex + 1 
+
+        if (currentIndex < 10 ) {
+            currentIndex = "0" + currentIndex
+        }
+           currentSlideEl.innerHTML = currentIndex
+
+        // Change Anchor - ex City Hall - Muchen
+        const anchorName = slides[this.realIndex].querySelector('.anchor-name')
+
+        currentAnchorEl.innerHTML = anchorName.innerHTML
+
+       }
+   }
+  
+  });
+
+  const slideCounter = () => {
+      const totalSlidesEl = document.querySelector('.slider__total')
+      const slides = document.querySelectorAll('.swiper-slide')
+
+    //  Det totala antalet slides (3) 
+      let totalSlides = slides.length - 2
+
+    // Ger oss nollan framför sifrran 3 
+      if (totalSlides < 10) {
+          totalSlides = "0" + totalSlides
+      }
+
+      totalSlidesEl.innerHTML = totalSlides 
+  }
+
+  slideCounter()
+  
